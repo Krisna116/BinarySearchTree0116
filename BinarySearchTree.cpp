82d23af0f422d64ec5bb03c9be9d0a1ce5d38cb3
Krisna <your-email@example.com>
2025-06-26 16:36:24 +0700

    Kelas BinaryTree untuk mengelola operasi pada tree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -16,3 +16,15 @@ public:
         rightchild = nullptr; // Inisialisasi anak kanan ke null
     }
 };
+
+// Kelas BinaryTree untuk mengelola operasi pada tree
+class BinaryTree
+{
+public:
+    Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = nullptr; // Inisialisasi ROOT ke null saat tree pertama kali dibuat
+    }
+};
